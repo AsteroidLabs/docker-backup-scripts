9cb91194de81aad6e81189a89566ab7ce8171ba3
Jacob Roy <jroy@shellomatic.com>
2020-02-25 10:16:33 -0500

    fix cron

--- cron.sh
+++ cron.sh
@@ -5,7 +5,7 @@
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "* * */1 * * /bin/bash $pwd/backup-all.sh" >> mycron
+echo "0 0 */1 * * /bin/bash $pwd/backup-all.sh" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
